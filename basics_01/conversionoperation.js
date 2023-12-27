bc66db8a64c01757a29bcd2463f4d0a3e3caf01f
Ripu0902 <82500521+Ripu0902@users.noreply.github.com>
2023-12-27 14:24:03 +0000

    conversionInOperation

--- basics_01/conversionoperation.js
+++ basics_01/conversionoperation.js
@@ -26,14 +26,38 @@ let isLoggedIn2 = 'Ripu';
 let isLoggedIn3 = '';
 
 let booleanIsLoggedIn1 = Boolean(isLoggedIn2);
-console.log(booleanIsLoggedIn1);
+// console.log(booleanIsLoggedIn1);
 
 let booleanIsLoggedIn2 = Boolean(isLoggedIn2);
-console.log(booleanIsLoggedIn2);
+// console.log(booleanIsLoggedIn2);
 
 let booleanIsLoggedIn3 = Boolean(isLoggedIn3);
-console.log(booleanIsLoggedIn3);
+// console.log(booleanIsLoggedIn3);
 
 // 1  => true
 // "" => false
 // "string" => true
+
+
+// ******************************Operations************************//
+
+// console.log(3 + 4);
+// console.log('3' + '4');
+// console.log('3' + 4);
+// console.log(3 + '4');
+// console.log('3' * '4');
+
+// console.log(true)
+// console.log(true+)  // error
+// console.log(+true)
+
+// console.log(+"")
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2+2;   / Not good way to assign
+
+let gameCounter = 100;
+console.log(gameCounter);
+console.log(++gameCounter);
+console.log(gameCounter);
+
