f0baba3fe8ced02437f33f0dd4078bc553910c2b
Ripu0902 <82500521+Ripu0902@users.noreply.github.com>
2023-12-27 13:41:56 +0000

    datatypes

---
+++ basics_01/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // Treat all Js cose as newer version
+
+// alert(3 + 3) // It basically used in  browser , we are using node js, not browser. although we can do this but in different way in node js.
+
+console.log(3 + 3);  // Code readability should be high
+console.log('RK');
+
+let name = 'Seema'
+let age =  21
+let isLoggedIn = false
+
+//number => 2 to the power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "Ritu"); //string
+console.log(typeof null); //Object
+console.log(typeof undefined); //undefined
+
