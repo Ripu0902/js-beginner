e1664e4ccec80d99ec7b3f78a7a8e182b2a62040
Ripu0902 <82500521+Ripu0902@users.noreply.github.com>
2023-12-27 14:05:49 +0000

    conversionIndatatypes

---
+++ basics_01/conversionoperation.js
@@ -0,0 +1,39 @@
+// let score1 = 95;
+// let score2 = "95";
+// let score3 = "95abc";
+// let score4 = null;
+// let score5 = undefined;
+// let score6 = true;
+
+// let valueInNumber1 = Number(score1);
+// let valueInNumber2 = Number(score2);
+// let valueInNumber3 = Number(score3);
+// let valueInNumber4 = Number(score4);
+// let valueInNumber5 = Number(score5);
+// let valueInNumber6 = Number(score6);
+
+
+// console.table([typeof(valueInNumber1),typeof(valueInNumber2),typeof(valueInNumber3),typeof(valueInNumber4),typeof(valueInNumber5),typeof(valueInNumber6)]);
+// console.table([valueInNumber1,valueInNumber2,valueInNumber3,valueInNumber4,valueInNumber5,valueInNumber6]);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+
+let isLoggedIn1 = 1;
+let isLoggedIn2 = 'Ripu';
+let isLoggedIn3 = '';
+
+let booleanIsLoggedIn1 = Boolean(isLoggedIn2);
+console.log(booleanIsLoggedIn1);
+
+let booleanIsLoggedIn2 = Boolean(isLoggedIn2);
+console.log(booleanIsLoggedIn2);
+
+let booleanIsLoggedIn3 = Boolean(isLoggedIn3);
+console.log(booleanIsLoggedIn3);
+
+// 1  => true
+// "" => false
+// "string" => true
